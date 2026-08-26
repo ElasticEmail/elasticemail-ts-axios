2494b233c08715ff8ab3d213edb106bf4c87b957
ElasticEmail <integrations@elasticemail.com>
2026-08-26 12:27:33 +0000

    Updated API

--- configuration.ts
+++ configuration.ts
@@ -92,7 +92,7 @@ export class Configuration {
         this.baseOptions = {
             headers: {
                 ...param.baseOptions?.headers,
-                'User-Agent': "OpenAPI-Generator/4.0.29/typescript-axios"
+                'User-Agent': "OpenAPI-Generator/4.0.30/typescript-axios"
             },
             ...param.baseOptions
         };
